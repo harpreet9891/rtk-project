119ee55c2413721ec852582e69acc67bd98c53be
harpreet9891 <hs370378@gmail.com>
2025-12-26 17:02:06 +0530

    Shopping Cart

---
+++ src/components/CardData.js
@@ -0,0 +1,112 @@
+const Cardsdata = [
+    {
+        id: 1,
+        dish: "Grilled Chicken Steak",
+        imgdata: "https://images.unsplash.com/photo-1604908176997-125f25cc6f3d",
+        address: "Continental, Grill",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png",
+        somedata: "1200+ orders placed recently",
+        price: 450,
+        rating: "4.2",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png",
+        qnty: 0
+    },
+    {
+        id: 2,
+        dish: "Cheese Burger",
+        imgdata: "https://images.unsplash.com/photo-1550547660-d9450f859349",
+        address: "Burger, Fast Food",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png",
+        somedata: "3000+ orders placed recently",
+        price: 180,
+        rating: "4.0",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png",
+        qnty: 0
+    },
+    {
+        id: 3,
+        dish: "Pepperoni Pizza",
+        imgdata: "https://images.pexels.com/photos/2619967/pexels-photo-2619967.jpeg",
+        address: "Pizza, Italian",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png",
+        somedata: "2100+ orders placed recently",
+        price: 320,
+        rating: "4.3",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png",
+        qnty: 0
+    },
+    {
+        id: 4,
+        dish: "Pasta Alfredo",
+        imgdata: "https://images.unsplash.com/photo-1621996346565-e3dbc646d9a9",
+        address: "Italian, Pasta",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png",
+        somedata: "950+ orders placed recently",
+        price: 280,
+        rating: "4.1",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png",
+        qnty: 0
+    },
+    {
+        id: 5,
+        dish: "Fish & Chips",
+        imgdata: "https://images.unsplash.com/photo-1541592106381-b31e9677c0e5",
+        address: "British, Seafood",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png",
+        somedata: "800+ orders placed recently",
+        price: 350,
+        rating: "3.9",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png",
+        qnty: 0
+    },
+    {
+        id: 6,
+        dish: "Chicken Sandwich",
+        imgdata: "https://images.unsplash.com/photo-1603048297172-c92544798d5a",
+        address: "Sandwich, Fast Food",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png",
+        somedata: "1600+ orders placed recently",
+        price: 150,
+        rating: "4.0",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png",
+        qnty: 0
+    },
+    {
+        id: 7,
+        dish: "BBQ Chicken Wings",
+        imgdata: "https://images.unsplash.com/photo-1606756790138-261d2b21cd75",
+        address: "BBQ, American",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png",
+        somedata: "1400+ orders placed recently",
+        price: 260,
+        rating: "4.2",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png",
+        qnty: 0
+    },
+    {
+        id: 8,
+        dish: "French Fries",
+        imgdata: "https://images.unsplash.com/photo-1573080496219-bb080dd4f877",
+        address: "Fast Food",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png",
+        somedata: "5000+ orders placed recently",
+        price: 120,
+        rating: "4.1",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png",
+        qnty: 0
+    },
+    {
+        id: 9,
+        dish: "Chocolate Brownie",
+        imgdata: "https://images.unsplash.com/photo-1606313564200-e75d5e30476c",
+        address: "Desserts, Bakery",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png",
+        somedata: "2200+ orders placed recently",
+        price: 160,
+        rating: "4.4",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png",
+        qnty: 0
+    }
+];
+
+export default Cardsdata;
